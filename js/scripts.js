08abfb3118bbdc71edafdc5b13e802ec884661c9
Taylor Somers <tsomers@yandex.com>
2020-06-01 17:43:44 -0400

    Create subtraction function

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,15 @@
 const add = function(number1, number2) {
   return number1 + number2;
   }
+
+  const subtract = function(number1, number2) {
+    return number1 - number2;
+    }
   
 const number1 = parseInt(prompt("Enter a number: "));
 const number2 = parseInt(prompt("Enter another number: "));
-const result = add(number, number2);
+const result = subtract(number1, number2);
   
-alert(add(number1, number2));
+alert(result);
 
 }
