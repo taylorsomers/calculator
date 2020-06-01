2f2f1e62a29625c38bc4b00913fd06102ba712cf
Taylor Somers <tsomers@yandex.com>
2020-06-01 17:46:12 -0400

    Create multiplication function in scripts js file

--- js/scripts.js
+++ js/scripts.js
@@ -7,10 +7,14 @@ const add = function(number1, number2) {
   const subtract = function(number1, number2) {
     return number1 - number2;
     }
+
+  const multiply = function(number1, number2) {
+    return number1 * number2;
+    }
   
 const number1 = parseInt(prompt("Enter a number: "));
 const number2 = parseInt(prompt("Enter another number: "));
-const result = subtract(number1, number2);
+const result = multiply(number1, number2);
   
 alert(result);
 
