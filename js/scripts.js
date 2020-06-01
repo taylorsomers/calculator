cf7bb2df450684a83cf2ebefb6d3c82bfbdf83ce
Taylor Somers <tsomers@yandex.com>
2020-06-01 17:25:11 -0400

    Add script tag to index html file and add js code from previous exercise to scripts js file

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+{
+
+const add = function(number1, number2) {
+  return number1 + number2;
+  }
+  
+const number1 = parseInt(prompt("Enter a number: "));
+const number2 = parseInt(prompt("Enter another number: "));
+const result = add(number, number2);
+  
+alert(add(number1, number2));
+
+}
