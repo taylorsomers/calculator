0a6e1416d41a4cbc6144d21e1e061dba23f7fb84
Taylor Somers <tsomers@yandex.com>
2020-06-01 17:48:46 -0400

    Create division function in scripts js file

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,21 @@ const add = function(number1, number2) {
   return number1 + number2;
   }
 
-  const subtract = function(number1, number2) {
-    return number1 - number2;
-    }
+const subtract = function(number1, number2) {
+  return number1 - number2;
+  }
 
-  const multiply = function(number1, number2) {
-    return number1 * number2;
-    }
+const multiply = function(number1, number2) {
+  return number1 * number2;
+  }
+
+const divide = function(number1, number2) {
+  return number1 / number2;
+}
   
 const number1 = parseInt(prompt("Enter a number: "));
 const number2 = parseInt(prompt("Enter another number: "));
-const result = multiply(number1, number2);
+const result = divide(number1, number2);
   
 alert(result);
 
